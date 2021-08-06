9726a9e9c95e8c634477f492de4b9a98b1b00450
choijiwoong <gogogi313@naver.com>
2021-08-06 20:43:33 +0900

    chap.13 finish (feat.polymorphism)

--- 씹어먹는 C++/13. 가상(virtual) 함수와 다형성.cpp
+++ 씹어먹는 C++/13. 가상(virtual) 함수와 다형성.cpp
@@ -97,6 +97,7 @@ int main(){
 	return 0;
 }*/
 
+/*4
 class EmployeeList{
 	int alloc_employee;
 	
@@ -120,7 +121,78 @@ class EmployeeList{
 	//so pointing Manager and Employee at Employee* is not suitable.
 	//In addition, calculate_pay() function will always work as Employee's calculate_pay() regardless of Manager.
 	//So sum of pay is calculated lower value than real value. because every manager is calculated by normal employee.
+};*/
+
+/*5
+class Base {
+
+ public:
+  Base() { std::cout << "기반 클래스" << std::endl; }
+
+  virtual void what() { std::cout << "기반 클래스의 what()" << std::endl; }//because of this!
 };
+class Derived : public Base {
+
+ public:
+  Derived() : Base() { std::cout << "파생 클래스" << std::endl; }
+
+  void what() { std::cout << "파생 클래스의 what()" << std::endl; }
+};
+int main() {
+  Base p;
+  Derived c;
+
+  Base* p_c = &c;//up casting
+  Base* p_p = &p;
+
+  std::cout << " == 실제 객체는 Base == " << std::endl;
+  p_p->what();
+
+  std::cout << " == 실제 객체는 Derived == " << std::endl;
+  p_c->what();//ddyong! true value print! not Base's what! because of^
+  //in upper code, compiler's interpret
+  //1. Let's do Base's what() because p_c is Base pointer!
+  //2. Wait, Base's what is virtual!
+  //3. Let's check is it real Base object! no! It's Derived object!
+  //4. Let's do Derived what!
+  //Mechanism is same when p_p->what().
+
+  return 0;
+}*/
+
+class Base {
+  std::string s;
+
+ public:
+  Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+
+  virtual void incorrect() { std::cout << "기반 클래스 " << std::endl; }
+};
+class Derived : public Base {
+  std::string s;
+
+ public:
+  Derived() : Base(), s("파생") {}
+
+  void incorrect() override { std::cout << "파생 클래스 " << std::endl; }//explicit override expression of Base class's virtual function. to privent this situation.
+};//actually it's different to Base's incorrect(). so add override keyword and delete const keyword
+int main() {
+  Base p;
+  Derived c;
+
+  Base* p_c = &c;
+  Base* p_p = &p;
+
+  std::cout << " == 실제 객체는 Base == " << std::endl;
+  p_p->incorrect();
+
+  std::cout << " == 실제 객체는 Derived == " << std::endl;
+  p_c->incorrect();
+  return 0;
+}
+  
+  
+
 
 /*
 [1.	is '-a'와 'has -s']
@@ -155,4 +227,26 @@ class EmployeeList{
 1.	Employee **employee_list에 Manager, employee를 몽땅 넣어버리자 by 업 캐스팅.->문제 발생. calculate를 Employee함수로 쓴다. 
 
 [5. virtual 키워드]	
+1.	EmployeeList 문제를 해결하기 이전에 간단한 예시로 살펴보자. 
+2.	virtual 키워드로 인하여 컴파일러가 실제 객체인지 확인을 한다. 이처럼 컴파일 시에 어떤 함수가 실행될 지 정해지지 않고, 런터임 시에 정해지는 것을 동적 바인딩(dynamic binding)이라고 한다.
+3.	동적 바인딩의 반대말로 정적 바인딩(static binding)이라는 말도 있지만, 이는 컴파일 타임에 어떤 함수가 호출될 지 정해지는 것으로 지금까지 사용해온 함수에 해당한다.
+4.	virtual키워드가 붙은 함수를 가상 함수(virtual function)이라고 한다.
+5.	파생 클래스의 함수가 기반 클래스의 함수를 오버라이드(override)하기 위해서는 두 함수의 꼴이 정확히 같아야 한다!
+
+[6.	override 키워드]
+1.	override 키워드를 사용하여 명시적으로 오버리아드를 알려주게되면, 실수로 오버라이드를 하지 않는 경우를 막을 수 있다. 
+
+[7. 그렇다면 Employee 문제를 어떻게 해결할까]
+1.	단순히 Employee 클래스의 calculate_pay함수와 print_info함수 앞에 virtual을 붙여서 컴파일러가 실제로 가리키는 형식이 어떤건지를 확인하게 하여 Employee와 Manager를 구분하게 한다. 
+2.	수정한 코드에서 이 부분을 보면, 
+    for (int i = 0; i < current_employee; i++) {
+      employee_list[i]->print_info();//이 부분은 employee_list가 manager나 employee냐에 따라서 다르게 동작하게 된다. 
+      total_pay += employee_list[i]->calculate_pay();
+    }
+	 즉, 같은 print_info함수를 호출했음에도 불구하고 경우에 따라 다른 것이 호출되는 일;
+	정리해서! 하나의 메소드를 호출했음에도 불구하고 여러가지 다른 작업들을 하는 것을 바로 다형성(polumorphism)이라고 부른다. 
+
+[8. 생각해보기]
+1.	virtual키워드의 처리과점. 이 포인터가 어떻게 객체를 가리키는지 어떻게 알 수 있을까 
 */
+
