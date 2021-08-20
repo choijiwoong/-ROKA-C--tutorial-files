8c4c8d741d97811fcf2f8502c5c09f60a3b2a178
choijiwoong <gogogi313@naver.com>
2021-08-20 22:11:49 +0900

    chap.18 오타 수정

--- 씹어먹는 C++/18. Excel만들기 프로젝트 2부.cpp
+++ 씹어먹는 C++/18. Excel만들기 프로젝트 2부.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include "utils.h"
-#include "utillity.cpp"
 
 int main(){
 	std::cout<<"테이블 (타입) (최대 행 크기) (최대 열 크기) 를 순서대로 입력해주세요"<<std::endl;
-	std<<cout<<"* 참고 *"<<std::endl;
+	std::cout<<"* 참고 *"<<std::endl;
 	std::cout<<"1: 텍스트 테이블, 2. CSV 테이블, 3: HTML 테이블"<<std::endl;
 	
-	itn type, max_row, max_col;
+	int type, max_row, max_col;
 	std::cin>>type>>max_row>>max_col;
-	MyExcel::excel(max_row, max_col, type-1);
+	MyExcel::Excel m(max_row, max_col, type-1);
 	m.command_line();//wait input and reprint when update of content
 }
 
