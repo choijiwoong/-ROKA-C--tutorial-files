7cca85dc19132cb38cd8f0b598deef3901e726dd
choijiwoong <gogogi313@naver.com>
2021-08-22 18:46:28 +0900

    chap.22 템플릿 메타 프로그래밍2 마무리 저녁식사 후

--- 씹어먹는 C++/22. 템플릿 메타 프로그래밍 2.cpp
+++ 씹어먹는 C++/22. 템플릿 메타 프로그래밍 2.cpp
@@ -36,6 +36,8 @@ struct check_div<N, N/2>{//stop condition
 	static const bool result=(N%(N/2)==0);
 };*/
 
+
+
 /*1
 template <int N>
 struct INT{
@@ -91,6 +93,8 @@ int main(){
 	std::cout<<"Is 11 prime? :: "<<is_prime<11>::result<<std::endl;//true
 	std::cout<<"Is 61 prime? :: "<<is_prime<61>::result<<std::endl;//true
 }*/
+
+
 
 //GCD define
 template <int X, int Y>
@@ -199,6 +203,14 @@ struct quantity{//class that save real data.
 	quantity(T q):q(q){}
 };
 
+//print quantity to ostream for convenience
+template <typename T, typename D>
+std::ostream& operator<<(std::ostream& out, const quantity<T, D>& q){
+	out<<q.q<<"kg^"<<D::M::num/D::M::den<<"m^"<<D::L::num/D::L::den<<"s^"<<D::T::num/D::T::den;
+	return out;
+}
+
+
 int main(){
 	using one=Ratio<1, 1>;
 	using zero=Ratio<0, 1>;
@@ -208,23 +220,58 @@ int main(){
 	quantity<double, Dim<zero, zero, one>> second(1);
 	
 	//good
-	kg+kg;
+	//kg+kg;
 	
 	//bad
-	kg+meter;
+	//kg+meter;
 	//error occur
 	//no opertor "+" matches these operands
 	//binary '+': no operator founs which takes a right-hand operand of type
 	//'quantity<double, Dim<zero, one, zero>>' (or there is no acceptable conversion)
 	
-	F=kg*meter/(second*second); //what type will be get F? F's dim is <Ratio<1, 1>, Ratio<1, 1>, Ratio<-2,1>>.
+	auto F=kg*meter/(second*second); //what type will be get F? F's dim is <Ratio<1, 1>, Ratio<1, 1>, Ratio<-2,1>>.
 	//So, F get 'quantity<double, Dim<one, one, Ratio<-1,1>>> F=kg*meter/(second*second);
 	//it's so annoying when we define variables. and compiler already know what type pop of F.
 	//So actually, we don't have to write that dirty type. so make compiler smart to think that type by self.
+	std::cout<<"2 kg 물체를 3m/s^2의 가속도로 밀기 위한 힘의 크기는? "<<F<<std::endl;
 }
+
+/*for understanding of auto keyword
+#include <typeinfo>
+
+int sum(int a, int b){
+	return a+b;
+} 
+
+class SomeClass{
+	int data;
+	
+	public:
+		SomeClass(int d):data(d){}
+		SomeClass(const SomeClass& s):data(s.data){}
+};
+
+int main(){
+	auto c=sum(1,2);//compiler can guess int by return type of function.
+	auto num=1.0+2.0;//can guess double
+	
+	SomeClass some(10);
+	auto some2=some;
+	
+	auto some3(10);//whether compiler makes SomeClass object? Compiler guess very simply, just make int variable.
+	//auto's guessing way is same to template type's guessing way.
+	//but like 'auto F=kg*meter/(second*second), compiler can guess type completely, we can use auto keyword.
+	
+	std::cout<<"C's type :: "<<typeid(c).name()<<std::endl;
+	std::cout<<"num's type :: "<<typeid(num).name()<<std::endl;
+	std::cout<<"some2's type :: "<<typeid(some2).name()<<std::endl;
+	std::cout<<"some3's type :: "<<typeid(some3).name()<<std::endl;
+}*/
 
 
 /*
+컴파일러 Tools->compiler option->add어쩌구->-std=c++14 이걸몰랐다니!!!럴수가!! 
+
 [1.	지난 강의 생각해보기 문제(TMP를 사용하여 어떤 수가 소수인지 아닌지를 판별하는 프로그램을 만들어보자)]
 1.	컴파일러의 구조상 어떠한 식별자(변수 이름이든 함수 이름이든 코드 상의 이름들)을 보았을 깨 이 식별자가 '값'인지 '타입'인지를 결정해야 한다.
 	예를들어 클래스에 대해 func함수가 특수화 한다면, T::t* p;가 클래스의 t와 p를 곱하는 것으로 해석이 되지만,
@@ -254,5 +301,18 @@ int main(){
 	 고로, 아예 단위가 다르면 컴파일시에 오류를 발생시켜버리는 것이 이상적이다. 
 
 [3. 타입을 알아서 추측해라! '-auto' 키워드]
-1.	 
+1.	객체를 생성할 때, 많은 경우 굳이 타입을 쓰지 않아도알아서 추측할 수 있는 경우들이 많이 있다.
+	(??) a=3;
+	some_class a;
+	(??) b=a;
+	객체가 복사생설될 때 도 복사 생성하는 대상의 타입을 확실히 알 수 있다면, 굳이 명시하지 않아도 컴파일러가 알 수있다.
+	물론 예외사항이 있는데, 위의 예제코드에서와 같이
+	quantity<double, Dim<one, zero, zero>> kg(1);
+	에서 만약 타입을 가리고 (??) kg(1);의 경우 컴파일러 입장에서는 그냥 1로 초기화하는 변수이므로 int로 추측할 것이다. 반면에,
+	(??) F=kg*meter/(second*second)에서는 굳이 타입을 적지 않아도 컴파일러가 오른쪽의 연산을 통해서 F의 타입을 정확하게 알아낼 수 있다.
+	 이처럼 컴파일러가 타입을 정확히 알아낼 수 있는 경우 굳이 그 길고 긴 타입을 적지않고 auto로 표현할 수 있다. 
+2.	짧은 이름의 타입의 경우 그냥 쓰는 것이 가독성측면에서 좋지만, 위처럼 복잡한 타입의 이름의 경우, 그 타입을 쉽게 추측할 수 있다면 auto키워드를 활용하는 것도 좋다.
+3.	사실 템플릿 메타프로그래밍의 경우 TMP특성상 복잡하고, 머리를 많이 써야되고, 디버깅이 힘들기에 현업에서 사용하는 경우는 그다지 많지 않다.
+	하지만 Unit클래스 처럼 TMP를 적절히 활용하면 런타임에서 찾아야 하는 오류를 컴파일 타임에서 미리 잡을 수 있고, 런타임 시 수행하는 연산들도 일부 컴파일 타임으로 옮길 수 있다.
+	또한 TMP를 작성할 일이 있을 때 비교적 쉽게 수행하기 위해 boost::MPL라이브러리가 있어 보다 쉽게 짤 수 있을 것이다. 
 */
