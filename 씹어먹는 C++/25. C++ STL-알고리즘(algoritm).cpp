6ea7c0997b30291af45e7039be7868f5f567a8c7
choijiwoong <gogogi313@naver.com>
2021-08-28 23:08:38 +0900

    chap.25 C++ STL-알고리즘(algorithm) 마무리

--- 씹어먹는 C++/25. C++ STL-알고리즘(algoritm).cpp
+++ 씹어먹는 C++/25. C++ STL-알고리즘(algoritm).cpp
@@ -107,7 +107,7 @@ int main(){
 	return 0;
 }*/
 
-//2
+/*2
 template<typename Iter>
 void print(Iter begin, Iter end){
 	while(begin!=end){
@@ -210,6 +210,192 @@ int main(){
 	//because in remove_if, functor can be copied any times by code.
 	
 	return 0;
+}*/
+
+/*3
+class SomeClass{
+	std::vector<int> vec;
+	int num_erased;
+	
+	public:
+		SomeClass(){
+			vec.push_back(5);
+			vec.push_back(3);
+			vec.push_back(1);
+			vec.push_back(2);
+			vec.push_back(3);
+			vec.push_back(4);
+			
+			num_erased=1;
+			
+			vec.erase(std::remove_if(vec.begin(), vec.end(),
+				[this](int i){//use this pointer not &num_erased! it's impossible because member variable doesn't has address before instantiation.
+					if(this->num_erased>=2)
+						return false;
+					else if(i%2==1){
+						this->num_erased++;
+						return true;
+					}
+					return false;
+				}),
+				vec.end()
+			);
+		}
+		
+}; 
+
+template <typename Iter>
+void print(Iter begin, Iter end){
+	while(begin!=end){
+		std::cout<<"["<<*begin<<"] ";
+		begin++;
+	}
+	std::cout<<std::endl;
+}
+
+int main(){
+	std::vector<int> vec;
+	vec.push_back(5);
+	vec.push_back(3);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	
+	std::cout<<"initial vec's status"<<std::endl;
+	print(vec.begin(), vec.end());
+	
+	std::cout<<"eleminate odd element in vector ---"<<std::endl;
+	int num_erased=0;
+	vec.erase(std::remove_if( vec.begin(), vec.end(), [&num_erased](int i)->bool{//By capture list, we can use num_erased like in scope. It means we can change value of num_erased in function.
+		//if we don't use & use just num_erased, it will be const. so we can't change that value, only read.
+		if(num_erased>=2)
+			return false;
+		else if(i%2==1){
+			num_erased++;
+			return true;
+		}
+		return false;
+	} ), vec.end());
+	
+	print(vec.begin(), vec.end());
+	
+	return 0;
+}*/
+
+//4
+template<typename Iter>
+void print(Iter begin, Iter end){
+	while(begin!=end){
+		std::cout<<"["<<*begin<<"] ";
+		begin++;
+	}
+	std::cout<<std::endl;
+} 
+/*4
+int main(){
+	std::vector<int> vec;
+	vec.push_back(5);
+	vec.push_back(3);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	
+	std::vector<int> vec2(6, 0);//initialize vec2 to '0' 6 times. 
+	
+	std::cout<<"initial vec's & vec2's status -----"<<std::endl;
+	print(vec.begin(), vec.end());
+	print(vec2.begin(), vec2.end());
+	
+	std::cout<<"add 1 to all element of vec and save to vec2"<<std::endl;
+	std::transform(vec.begin(), vec.end(), vec2.begin(), [](int i) {return i+1;});//not need for loop! and high read ability!
+	print(vec.begin(), vec.end());
+	print(vec2.begin(), vec2.end());
+	
+	return 0;
+}*/
+
+/*5-1
+int main(){
+	std::vector<int> vec;
+	vec.push_back(5);
+	vec.push_back(3);
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	
+	auto result=std::find(vec.begin(), vec.end(), 3);
+	std::cout<<"3 is "<<std::distance(vec.begin(), result)+1<<"st element"<<std::endl;
+	
+	//if you want to find all e element
+	auto current=vec.begin();
+	while(true){
+		current=std::find(current, vec.end(), 3);
+		if(current==vec.end())
+			break;
+		std::cout<<"3 is "<<std::distance(vec.begin(), current)+1<<"st element"<<std::endl;
+		current++;
+	}
+	
+	//find_if function
+	auto current2=vec.begin();
+	while(true){
+		current2=std::find_if(current2, vec.end(), [](int i)->int{return i%3==2;});//find_if function get functor as argument
+		if (current2==vec.end())
+			break;
+		std::cout<<"element that satisfies i%3==2 is : "<<*current2<<std::endl;
+		current2++;
+	} 
+	
+	return 0;
+}*/
+
+//5-2
+struct User{
+	std::string name;
+	int level;
+	
+	User(std::string name, int level) : name(name), level(level) {}
+	bool operator==(const User& user) const{
+		if(name==user.name && level==user.level)
+			return true;
+		return false;
+	}
+};
+
+class Party{
+	std::vector<User> users;
+	
+	public:
+		bool add_user(std::string name, int level){
+			User new_user(name, level);
+			if(std::find(users.begin(), users.end(), new_user) != users.end())
+				return false;//overlap
+			users.push_back(new_user);
+			return true;
+		}
+		bool can_join_dungeon(){//all >15 level
+			return std::all_of(users.begin(), users.end(), [](User& user) {return user.level>=15;});
+		}
+		bool can_use_special_item(){//anyone >19 level
+			return std::any_of(users.begin(), users.end(), [](User& user){return user.level>=19;});
+		}
+};
+int main(){
+	Party party;
+	party.add_user("covid",19);
+	party.add_user("column",0);
+	party.add_user("row",1);
+	party.add_user("cookie", 10);
+	party.add_user("covid", 19);
+	
+	std::cout<<std::boolalpha;
+	std::cout<<"can join dungeon? "<<party.can_join_dungeon()<<std::endl;
+	std::cout<<"can use special item? "<<party.can_use_special_item()<<std::endl;
+	
+	return 0;
 }
 
 
@@ -272,5 +458,31 @@ ForwardIt remove_if(ForwardIt first, ForwardIt last, UnaryPredicate pred) {
 4.	고로, num_delete를 객체 내부 변수가 아닌 외부 변수로 빼야 이 문제를 해결할 수 있다.
 	한가지 안좋은 점은 STL을 사용할 때 마다 외부에 클래스나 함수를 하나씩 만들어줘야 한다는 것이다. 고로 나중에 코드가커졌을 때, 가독성이 떨어질 수 있어
 	STL알고리즘 그 안에 직접 써놓는 것이 이상적인 방법이다. 
-	 
+
+[3.	람다 함수(lambda function)]
+1.	[capture list] (받는 인자)->리턴 타입{함수 본체} 으로 정의되며, 리턴 타입을 생략하면 알아서 컴파일러가 추측해 줌. but return이 여러개여 추측할 수 없다면 컴파일오류 
+	이는 auto func=로 받아서 func(4)처럼 함수 객체를 생성한 후에 호출할 수도 있음. 즉, 말 그대로 함수 임. 
+	 하지만, 위에서 발생한 문제를 다시 살펴보면, 함수를 재호출하면 함수 안의 변수가 재정의되어 결국 odd element를 몇 개 지웠는지 모른다는 점이 여전할 것 같지만,
+	lambda함수는 캡쳐 목록(capture list)를 사용하여 그 변수에 접근할 수 있다. 
+2.	만약 클래스 안에 멤버 함수 안에서 멤버 변수를 참조할 때는, 조금 다른게 위처럼 &num_erased라고 하면 안된다. 우리는 실제 생성된 객체 안의 멤버를 참조해야하기에
+	이러한 경우엔 this로 class자체를 나타내어 사용한다. 
+3.	캡쳐리스트의 사용법은 몇개만 간단히 정리하면
+	-[] 아무것도 캡쳐 안함
+	-[&a, b] a는 레퍼런스로 캡쳐, b는 복사본으로 캡쳐(변경 불가능)
+	-[&] 외부의 모든 변수들을 레퍼런스로 캡쳐
+	-[=] 외부의 모든 변수들을 복사본으로 캡쳐 
+	
+[4.	원소 수정하기(transform)]
+1.	transform 함수를 사용하여 원소들을 수정. 
+2.	transform(시작 반복자, 끝 반복자, 결과를 저장할 컨테이너의 시작 반복자, Pred) 
+	
+[5.	원소를 탐색하는 함수(find, 'find_if, any_of, all_od' 등등)]
+1.	template <class InputIt, class T>
+	InputIt find(InputIt first, InputIt last, const T& value)
+	first~last중 value와 같은게 있는지 확인하고 있으면 반복자 리턴. 중복되면 가장 최근꺼. 
+2.	find 계열 함수들을 사용할 때 주의할 점은, 만약 기본적으로 .find함수가 지원된다면 이를 사용해야 더 빠르다. 왜냐면 알고리즘의 find는 컨테이너의 구조를 고려하지 않는 find기 때문이다.
+	ex) set의 find는 정렬되어있기에 O(logn), unordered_set의 find는 해시테이블을 이용하기에 O(1). 만약 알고리즘의 find는 하나씩 확인하므로 O(n).
+	 즉, 알고리즘의 find는 vector처럼 find를 지원하지 않는 컨테이너에 사용하자! 
+3.	any_of는 인자로 받은 범위에서 하나라도 충족하면 true리턴, all_of는 모두가 충족하면 true를 리턴한다. and와 or과 비슷한 느낌이다.
+ 
 */
