2973d8d6cd2a92e57c2abf4d1616bc15c0b55d2f
choijiwoong <gogogi313@naver.com>
2022-01-09 16:09:41 +0900

    탁상용 계산기

--- The Programming Language C++/탁상용 계산기.cpp
+++ The Programming Language C++/탁상용 계산기.cpp
@@ -34,12 +34,38 @@ primary:
 
 	[저수준 입력]
 1.	출력을 위한 ;, 공백을 넣어 x =7꼴로 만들어야 하는 것들은 귀찮기에 get을 발전시켜보자. 
+
+	[오류처리]
+1.	오류의 개수를 세며, 출력한다음 결과를 반환하는 error함수를 세팅했다. 
+2.	범용의 오류 처리 메커니즘은 오류탐지와 오류 복구로 구분되지만, 우리 탁상용 계산기의 수준에서는 stream::get()이 행 번호를 기억하고 있다는 가정 하에 오류의 발생위치를 사용자에 알릴 수 있기에 이정도도 적당하다. 
+
+	[드라이버]
+1.	시작을 위한 드라이브로 준비와 오류보고를 담당할 main, 실제 계산을 처리할 calculate()를 사용한다. 
+
+	[커맨드라인 인자]
+1.	우리가 평가한 expression이 command-line argument로 입력될 수 있다면 몇개의 키를 덜 눌러도 되기에 발전시켜보자
+2.	main을 호출하는 규약은 C와 공통적이기에 C스타일 문자열이 사용된다. 고로 입력스트림에서 읽어들이는 것과 같은 방식으로 명령 문자열에서 읽어들이자. Token_stream이 적합한 istringstream에서 읽게 하면 된다. 
 */
-enum class Kind: char{
+
+#include <iostream>//io
+#include <string>
+#include <map>
+#include <cctype>//isalpha()
+
+using namespace std;
+
+//definition
+double error(const string& s);//increase global variable no_of_errors and print current num of errors and return 1
+enum class Kind: char{//for expression name, number, end, operators for parsing
 	name, number, end,
 	plus='+', minus='-', mul='*', div='/', print=';', assign='=', lp='(', rp=')'
 };
-struct Token_stream{
+struct Token{//it can get string_value or number_value by it's Kind(enum) member
+	Kind kind;
+	string string_value;
+	double number_value;
+};
+struct Token_stream{//it provides get(), current(), set_input(istream& s), set_input(istream* p). we have to judge is it pointer or reference by it's member owns
 	public:
 		Token_stream(istream& s): ip{&s}, owns{false}{}
 		Token_stream(istream* p): ip{p}, owns{true}{}
@@ -53,11 +79,33 @@ struct Token_stream{
 		void set_input(istream* p){ close(); ip=p; owns=true; }
 	
 	private:
-		void close(){ id(owns) delete ip; }
+		void close(){ if(owns) delete ip; }
 		istream* ip;
 		bool owns;//ip가 &인지 *인지. 
 		Token ct{Kind::end};//초기값 for 사용자 실수(current token)
 };
+double prim(bool get);//handling primary, read and handling TokenStream by it's Token by using ts.current(). (ts is global vaiable of Token_stream)
+double term(bool get);//* & /
+double expr(bool get);//+ & -
+void calculate();//call expr function properly.
+int main(int argc, char* args[]);//use command-line argument
+
+
+
+//global variables
+int no_of_errors;//[오류 처리]error function like exception object.
+Token_stream ts{cin};//use input of cin
+map<string, double> table;
+
+
+
+double error(const string& s){
+	no_of_errors++;
+	cerr<<"error: "<<s<<'\n';//cerr is unbuffered stream so it's used for reporting error.
+	return 1;
+}
+
+//inner structure
 Token Token_stream::get(){//[입력]
 	char ch=0;
 	//[저수준 입력]
@@ -75,8 +123,7 @@ Token Token_stream::get(){//[입력]
 		
 		case 0://read null
 			return ct={Kind::end};//assign & return
-		
-		case ';':
+			
 		case '*':
 		case '/':
 		case '+':
@@ -86,7 +133,7 @@ Token Token_stream::get(){//[입력]
 		case '=':
 			return ct={static_cast<Kind>(ch)};//assign & return to enum
 		
-		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': cast '9': case '.':
+		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.':
 			ip->putback(ch);//다시 넣기
 			*ip>>ct.number_value;//숫자를 읽기
 			ct.kind=Kind::number;//set ct
@@ -115,6 +162,7 @@ Token Token_stream::get(){//[입력]
 	}
 }
 
+//functions
 double prim(bool get){
 	if(get)
 		ts.get();//다음 토큰 읽기 
@@ -167,7 +215,8 @@ double term(bool get){
 	}
 } 
 
-double expr(bool get){
+
+double expr(bool get){//[드라이버] 
 	double left=term(get);
 	for(;;){
 		switch(ts.current().kind){
@@ -181,4 +230,38 @@ double expr(bool get){
 				return left;
 		}
 	}
+}
+
+
+
+void calculate(){
+	for(;;){
+		ts.get();//user input(correctly, std input)
+		if(ts.current().kind==Kind::end) break;
+		if(ts.current().kind==Kind::print) continue;
+		
+		cout<<expr(false)<<'\n';
+	}
+}
+
+int main(int argc, char* argv[]){
+	switch(argc){//for using command-line argument
+		case 1:
+			break;
+		
+		case 2:
+			ts.set_input(new istringstream(argv[1]));
+			break;
+			
+		default:
+			error("too many arguments");
+			return 1;
+	}
+	
+	table["pi"]=3.1415926535897932385;//rounded value
+	table["e"]=2.7182818284590452354;
+	
+	calculate();
+	
+	return no_of_errors;//num of error
 }
